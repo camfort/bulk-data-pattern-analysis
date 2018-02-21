013ebb54860ce5d0b0d866de77c5d70a4d77e09f
Dominic Orchard <dom.orchard@gmail.com>
2018-02-21 17:50:28 +0000

    stubs

---
+++ src/Analysis.hs
@@ -0,0 +1,11 @@
+-- Core analysis
+
+module Analysis where
+
+import qualified Language.Fortran.AST as F
+import qualified Language.Fortran.Analysis as FA
+import qualified Language.Fortran.Analysis.Types as FAT
+import qualified Language.Fortran.Analysis.Renaming as FAR
+import qualified Language.Fortran.Analysis.BBlocks as FAB
+import qualified Language.Fortran.Analysis.DataFlow as FAD
+import qualified Language.Fortran.Util.Position as FU
