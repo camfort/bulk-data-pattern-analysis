a7084baf01afb8a392f6f16c111afd48b12e838e
Dominic Orchard <dom.orchard@gmail.com>
2018-02-22 14:14:43 +0000

    working stub

--- src/Analysis.hs
+++ src/Analysis.hs
@@ -9,3 +9,35 @@ import qualified Language.Fortran.Analysis.Renaming as FAR
 import qualified Language.Fortran.Analysis.BBlocks as FAB
 import qualified Language.Fortran.Analysis.DataFlow as FAD
 import qualified Language.Fortran.Util.Position as FU
+
+import Camfort.Analysis.Annotations  (A, unitAnnotation)
+import Camfort.Helpers
+import Data.Monoid
+
+
+-- High-level idea-
+--    There are two categories:
+--          1 - A program with global array that is indexed by global variables
+--          2 - A program with stencil access with a single region forward
+--              (matching the size of the stride)
+-- Metrics:
+--    What proportion of program units is this used in
+--    How large is the array
+--    How many variables used as subscripts
+--    (Tentative) Coverage of subscripts
+--    (Tentative) Overlapping subscripts
+
+data AnalysisInfo = AnalysisInfo
+  {
+    categoryOne :: [(Filename, FU.Position)]
+  }
+
+instance Monoid AnalysisInfo where
+  mempty = AnalysisInfo { categoryOne = [] }
+  mappend x y =
+    AnalysisInfo {
+      categoryOne = categoryOne x ++ categoryOne y
+    }
+
+analysis :: [(F.ProgramFile A, SourceText)] -> AnalysisInfo
+analysis _ = mempty
--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,13 @@
 -- Analysis for finding bulk data patterns in Fortran code
 
--- High-level idea-
---    There are two categories:
---          0 - A program with global array that is indexed by global variables
---          1 - A program with stencil access with a single region forward
---              (matching the size of the stride)
--- Metrics:
---    What proportion of program units is this used in
---    How large is the array
---    How many variables used as subscripts
---    (Tentative) Coverage of subscripts
---    (Tentative) Overlapping subscripts
 
 module Main where
 
 import Analysis
-import Camfort.Analysis.Input
+import Camfort.Input
 
 main :: IO ()
 main = do
-  file <- readParseSrcDir "exmaple.f90" []
+  let modFiles = []
+  file <- readParseSrcDir modFiles "exmaple.f90" []
   return ()
